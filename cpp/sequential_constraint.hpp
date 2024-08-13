189bd647fdac034cf3dde07400bf717f2db47002
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-13 16:40:34 +0900

    fix: handle case if t=0 has no constraint

--- cpp/sequential_constraint.hpp
+++ cpp/sequential_constraint.hpp
@@ -9,9 +9,10 @@ using SMatrix = Eigen::SparseMatrix<double, Eigen::ColMajor>;
 class SequentialCst {
  public:
   using Ptr = std::shared_ptr<SequentialCst>;
-  SequentialCst(size_t T)
+  SequentialCst(size_t T, size_t q_dim)
       : T_(T),
         cst_dim_(0),
+        q_dim_(q_dim),
         constraints_seq_(T),
         fixed_points_(T, std::nullopt),
         finalized_(false),
@@ -23,13 +24,13 @@ class SequentialCst {
   void add_motion_step_box_constraint(const Eigen::VectorXd& box_width);
   void finalize();
   std::pair<Eigen::VectorXd, SMatrix> evaluate(const Eigen::VectorXd& x);
-  inline size_t x_dim() const { return q_dim() * T_; }
-  inline size_t q_dim() const { return constraints_seq_[0][0]->q_dim(); }
+  inline size_t x_dim() const { return q_dim_ * T_; }
   inline size_t cst_dim() const { return cst_dim_; }
 
  private:
   size_t T_;
   size_t cst_dim_;
+  size_t q_dim_;
   std::vector<std::vector<ConstraintBase::Ptr>> constraints_seq_;
   std::vector<std::optional<Eigen::VectorXd>> fixed_points_;
   bool finalized_;
