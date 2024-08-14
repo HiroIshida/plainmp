e1edbe7dfaff8bd41744a772091498f044955c9e
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-15 07:15:05 +0900

    maint: include submodules in package

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 try:
     from skbuild import setup
+    from setuptools import find_packages
 except ImportError:
     raise Exception
 
@@ -10,7 +11,7 @@ setup(
     author="Hirokazu Ishida",
     license="PolyForm NonCommercial 1.0.0",
     install_requires=["numpy", "scipy", "scikit-robot", "pyyaml", "ompl-thin", "robot_descriptions", "osqp"],
-    packages=["plainmp"],
+    packages=find_packages(where="python"),
     package_dir={"": "python"},
     package_data={"plainmp": ["*.pyi", "conf/*.yaml"]},
     cmake_install_dir="python/plainmp/",
