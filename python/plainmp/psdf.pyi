24a717e4064bc16a061e14cf27421ef99e2774df
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-15 09:42:03 +0900

    docs: fix docstrings for psdf

--- python/plainmp/psdf.pyi
+++ python/plainmp/psdf.pyi
@@ -6,16 +6,22 @@ class Pose:
     def __init__(self, translation: np.ndarray, rotation: np.ndarray) -> None: ...
 
 class SDFBase:
-    def evaluate(self, points: np.ndarray) -> np.ndarray:
+    def evaluate(self, point: np.ndarray) -> np.ndarray:
         """Evaluate the SDF at the given points.
         Args:
-            points: The (3, n_pts) points to evaluate the SDF at.
-                TODO: Should this be a (n_pts, 3) array for consistency with numpy?
-                This would require changing Eigen's default storage order to be row-major.
+            point: The (3,) point to evaluate the SDF at.
         Returns:
-            The signed distance at each point.
+            The signed distance at
         """
-    ...
+        ...
+    def evaluate_batch(self, points: np.ndarray) -> np.ndarray:
+        """Evaluate the SDF at the given points.
+        Args:
+            points: The (N, 3) points to evaluate the SDF at.
+        Returns:
+            The signed distances at the given points.
+        """
+        ...
 
 class UnionSDF(SDFBase):
     def __init__(self, sdf_list: List[SDFBase], create_bvh: bool) -> None: ...
