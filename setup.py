1c619c65006bf49811fc6a247db7ce4778736289
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-19 17:56:00 +0900

    0.0.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name="plainmp",
-    version="0.0.5",
+    version="0.0.6",
     description="experimental",
     author="Hirokazu Ishida",
     license="PolyForm NonCommercial 1.0.0",
