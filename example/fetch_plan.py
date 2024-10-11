7024d915338b208cf6d7bcfae51f296e7dc96de6
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-11 13:46:13 +0900

    example: add example using pcloud

--- example/fetch_plan.py
+++ example/fetch_plan.py
@@ -2,13 +2,13 @@ import argparse
 import time
 
 import numpy as np
-from skrobot.model.primitives import Box
+from skrobot.model.primitives import Box, PointCloudLink
 from skrobot.models import Fetch
 from skrobot.viewers import PyrenderViewer
 
 from plainmp.ompl_solver import OMPLSolver
 from plainmp.problem import Problem
-from plainmp.psdf import UnionSDF
+from plainmp.psdf import CloudSDF, UnionSDF
 from plainmp.robot_spec import FetchSpec
 from plainmp.utils import set_robot_state, sksdf_to_cppsdf
 
@@ -24,21 +24,32 @@ except ImportError:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--pcloud", action="store_true", help="use point cloud for collision check")
     parser.add_argument("--visualize", action="store_true", help="visualize the result")
     args = parser.parse_args()
 
     fs = FetchSpec()
     cst = fs.create_collision_const()
 
-    table = Box([1.0, 2.0, 0.05], with_sdf=True)
-    table.translate([1.0, 0.0, 0.8])
+    if args.pcloud:
+        table_points = np.random.rand(10000, 3) * np.array([1.0, 2.0, 0.05]) - np.array(
+            [0.5, 1.0, 0.025]
+        )
+        table_points += np.array([0.9, 0.0, 0.8])
+        table = PointCloudLink(table_points)
+        table_sdf = CloudSDF(table_points, 0.002)
+    else:
+        table = Box([1.0, 2.0, 0.05], with_sdf=True)
+        table.translate([0.9, 0.0, 0.8])
+        table_sdf = sksdf_to_cppsdf(table.sdf)
+
     ground = Box([2.0, 2.0, 0.05], with_sdf=True)
-    sdf = UnionSDF([sksdf_to_cppsdf(table.sdf), sksdf_to_cppsdf(ground.sdf)], False)
+    sdf = UnionSDF([table_sdf, sksdf_to_cppsdf(ground.sdf)], False)
     cst.set_sdf(sdf)
     lb, ub = fs.angle_bounds()
     start = np.array([0.0, 1.31999949, 1.40000015, -0.20000077, 1.71999929, 0.0, 1.6600001, 0.0])
     goal = np.array([0.386, 0.20565, 1.41370, 0.30791, -1.82230, 0.24521, 0.41718, 6.01064])
-    msbox = np.array([0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.2, 0.2])
+    msbox = np.ones(8) * 0.05
     problem = Problem(start, lb, ub, goal, cst, None, msbox)
     solver = OMPLSolver()
 
