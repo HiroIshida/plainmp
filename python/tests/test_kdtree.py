f5fa535f8a72000f8d32cb0bdc58ca0374dfcdac
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-17 02:52:15 +0900

    test: fix kdtree test

--- python/tests/test_kdtree.py
+++ python/tests/test_kdtree.py
@@ -5,7 +5,7 @@ from plainmp.kdtree import KDTree
 
 def test_kdtree():
     points = np.random.randn(1000, 3)
-    tree = KDTree(points, 0.0)
+    tree = KDTree(points)
 
     def blute_force(points, query):
         idx = np.argmin(np.sum((points - query) ** 2, axis=1))
