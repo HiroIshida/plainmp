024bc35dfeea91ef9709a4cb90af6ac783f62578
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-11-29 19:42:52 +0900

    refactor(ompl): algorithm selector

--- cpp/ompl/algorithm_selector.cpp
+++ cpp/ompl/algorithm_selector.cpp
@@ -17,8 +17,8 @@ namespace ob = ompl::base;
 namespace og = ompl::geometric;
 
 template <typename T>
-std::shared_ptr<T> create_algorithm(const ob::SpaceInformationPtr si,
-                                    std::optional<double> range) {
+std::shared_ptr<T> create_with_range(const ob::SpaceInformationPtr& si,
+                                     std::optional<double> range) {
   auto algo = std::make_shared<T>(si);
   if (range) {
     algo->setRange(*range);
@@ -26,37 +26,34 @@ std::shared_ptr<T> create_algorithm(const ob::SpaceInformationPtr si,
   return algo;
 }
 
-std::shared_ptr<ob::Planner> get_algorithm(const std::string& name,
-                                           const ob::SpaceInformationPtr& si,
-                                           std::optional<double> range) {
-  if (name.compare("BKPIECE1") == 0) {
-    return create_algorithm<og::BKPIECE1>(si, range);
-  } else if (name.compare("LBKPIECE1") == 0) {
-    return create_algorithm<og::LBKPIECE1>(si, range);
-  } else if (name.compare("KPIECE1") == 0) {
-    return create_algorithm<ocustom::KPIECE1Modified>(si, range);
-  } else if (name.compare("RRT") == 0) {
-    return create_algorithm<ocustom::RRTModified>(si, range);
-  } else if (name.compare("RRTConnect") == 0) {
-    return create_algorithm<og::RRTConnect>(si, range);
-  } else if (name.compare("RRTstar") == 0) {
-    return create_algorithm<og::RRTstar>(si, range);
-  } else if (name.compare("EST") == 0) {
-    return create_algorithm<og::EST>(si, range);
-  } else if (name.compare("BiEST") == 0) {
-    return create_algorithm<og::BiEST>(si, range);
-  } else if (name.compare("AITstar") == 0) {
+std::shared_ptr<ompl::base::Planner> get_algorithm(
+    const std::string& name,
+    const ompl::base::SpaceInformationPtr& si,
+    std::optional<double> range) {
+  // clang-format off
+    if (name == "BKPIECE1") return create_with_range<og::BKPIECE1>(si, range);
+    if (name == "LBKPIECE1") return create_with_range<og::LBKPIECE1>(si, range);
+    if (name == "KPIECE1") return create_with_range<ocustom::KPIECE1Modified>(si, range);
+    if (name == "RRT") return create_with_range<ocustom::RRTModified>(si, range);
+    if (name == "RRTConnect") return create_with_range<og::RRTConnect>(si, range);
+    if (name == "RRTstar") return create_with_range<og::RRTstar>(si, range);
+    if (name == "EST") return create_with_range<og::EST>(si, range);
+    if (name == "BiEST") return create_with_range<og::BiEST>(si, range);
+  // clang-format on
+
+  if (name == "AITstar" || name == "AITstarStop") {
     return std::make_shared<og::AITstar>(si);
-  } else if (name.compare("AITstarStop") == 0) {
-    return std::make_shared<og::AITstar>(si);
-  } else if (name.compare("BITstar") == 0) {
+  }
+  if (name == "BITstar") {
     return std::make_shared<og::BITstar>(si);
-  } else if (name.compare("BITstarStop") == 0) {
+  }
+  if (name == "BITstarStop") {
     auto bit = std::make_shared<og::BITstar>(si);
     bit->setStopOnSolnImprovement(true);
     return bit;
   }
-  throw std::runtime_error("algorithm " + name + " is not supported");
+  throw std::runtime_error(std::string("Algorithm '") + std::string(name) +
+                           "' is not supported");
 }
 
 }  // namespace plainmp::ompl_wrapper
--- cpp/ompl/algorithm_selector.hpp
+++ cpp/ompl/algorithm_selector.hpp
@@ -1,10 +1,11 @@
 #include <ompl/geometric/planners/PlannerIncludes.h>
+#include <optional>
 
 namespace plainmp::ompl_wrapper {
 
 std::shared_ptr<ompl::base::Planner> get_algorithm(
     const std::string& name,
     const ompl::base::SpaceInformationPtr& si,
-    std::optional<double> range);
+    std::optional<double> range = std::nullopt);
 
 }  // namespace plainmp::ompl_wrapper
