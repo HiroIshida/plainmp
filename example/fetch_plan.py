7ede550e254842069d81cd98a34853485a5f09e9
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-12-01 00:11:36 +0900

    refactor(example): fetch_plan.py

--- example/fetch_plan.py
+++ example/fetch_plan.py
@@ -2,25 +2,14 @@ import argparse
 import time
 
 import numpy as np
-from skrobot.model.primitives import Box, PointCloudLink
-from skrobot.models import Fetch
+from skrobot.model.primitives import Box, PointCloudLink, Sphere
 from skrobot.viewers import PyrenderViewer
 
-from plainmp.ompl_solver import OMPLSolver
+from plainmp.ompl_solver import OMPLSolver, set_log_level_none
 from plainmp.problem import Problem
-from plainmp.psdf import CloudSDF, UnionSDF
+from plainmp.psdf import CloudSDF, GroundSDF, UnionSDF
 from plainmp.robot_spec import FetchSpec
 from plainmp.utils import set_robot_state, sksdf_to_cppsdf
-
-try:
-    from skmp.robot.fetch import FetchConfig
-    from skmp.visualization.collision_visualizer import (
-        CollisionSphereVisualizationManager,
-    )
-
-    SKMP_AVAILABLE = True
-except ImportError:
-    SKMP_AVAILABLE = False
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -28,6 +17,7 @@ if __name__ == "__main__":
     parser.add_argument("--visualize", action="store_true", help="visualize the result")
     args = parser.parse_args()
 
+    set_log_level_none()
     fs = FetchSpec()
     cst = fs.create_collision_const()
 
@@ -35,50 +25,41 @@ if __name__ == "__main__":
         table_points = np.random.rand(10000, 3) * np.array([1.0, 2.0, 0.05]) - np.array(
             [0.5, 1.0, 0.025]
         )
-        table_points += np.array([0.9, 0.0, 0.8])
+        table_points += np.array([0.95, 0.0, 0.8])
         table = PointCloudLink(table_points)
         table_sdf = CloudSDF(table_points, 0.002)
     else:
-        table = Box([1.0, 2.0, 0.05], with_sdf=True)
-        table.translate([0.9, 0.0, 0.8])
+        table = Box([1.0, 2.0, 0.05], face_colors=[100, 200, 100, 200], with_sdf=True)
+        table.translate([0.95, 0.0, 0.8])
         table_sdf = sksdf_to_cppsdf(table.sdf)
 
-    ground = Box([2.0, 2.0, 0.05], with_sdf=True)
-    sdf = UnionSDF([table_sdf, sksdf_to_cppsdf(ground.sdf)], False)
+    sdf = UnionSDF([table_sdf, GroundSDF(0.0)], False)
     cst.set_sdf(sdf)
     lb, ub = fs.angle_bounds()
-    start = np.array([0.0, 1.31999949, 1.40000015, -0.20000077, 1.71999929, 0.0, 1.6600001, 0.0])
-    goal = np.array([0.386, 0.20565, 1.41370, 0.30791, -1.82230, 0.24521, 0.41718, 6.01064])
-    msbox = np.ones(8) * 0.05
-    problem = Problem(start, lb, ub, goal, cst, None, msbox)
+    q_start = np.array([0.0, 1.32, 1.40, -0.20, 1.72, 0.0, 1.66, 0.0])
+    q_goal = np.array([0.386, 0.205, 1.41, 0.308, -1.82, 0.245, 0.417, 6.01])
+    resolution = np.ones(8) * 0.05
+    problem = Problem(q_start, lb, ub, q_goal, cst, None, resolution)
     solver = OMPLSolver()
-
     ret = solver.solve(problem)
-    assert ret.traj is not None
-    print(f"planning time {1000 * (ret.time_elapsed)} [ms]")
+    print(f"solved in {ret.time_elapsed * 1000:.2f} ms")
 
     if args.visualize:
-        fetch = Fetch()
-        set_robot_state(fetch, fs.control_joint_names, goal)
+        robot_model = fs.get_robot_model(with_mesh=True)
+
+        # get goal point
+        set_robot_state(robot_model, fs.control_joint_names, q_goal)
+        co = robot_model.gripper_link.copy_worldcoords()
+
         v = PyrenderViewer()
-
-        if SKMP_AVAILABLE:
-            conf = FetchConfig()
-            colkin = conf.get_collision_kin()
-            colvis = CollisionSphereVisualizationManager(colkin, v)
-            colvis.update(fetch)
-
-        v.add(fetch)
+        v.add(Sphere(0.05, color=[255, 0, 0]).translate(co.worldpos()))
+        v.add(robot_model)
         v.add(table)
-        v.add(ground)
+        v.add(Box([2.0, 2.0, 0.05]))  # ground
         v.show()
-
         time.sleep(1.0)
-        for q in ret.traj.resample(20):
-            set_robot_state(fetch, fs.control_joint_names, q)
-            if SKMP_AVAILABLE:
-                colvis.update(fetch)
+        for q in ret.traj.resample(50):
+            set_robot_state(robot_model, fs.control_joint_names, q)
             v.redraw()
-            time.sleep(0.5)
-
+            time.sleep(0.2)
         time.sleep(1000)
