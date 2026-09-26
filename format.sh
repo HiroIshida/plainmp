e179ca2cdbcd07163c5871bee13b659431b3cb94
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2026-09-26 18:30:02 +0900

    chore: migrate Python workflows to uv (#91)

--- format.sh
+++ format.sh
@@ -7,11 +7,11 @@ if [[ "$("$clang_format" --version)" != *"version 14."* ]]; then
 fi
 find cpp -type f \( -name "*.cpp" -o -name "*.hpp" \) | xargs "$clang_format" -i -style=Chromium
 
-find . -name "*py"|xargs python3 -m autoflake -i --remove-all-unused-imports --remove-unused-variables --ignore-init-module-imports
+find src/plainmp tests example -type f -name "*.py" -print0 | xargs -0 python3 -m autoflake -i --remove-all-unused-imports --remove-unused-variables --ignore-init-module-imports
 for module in "src/plainmp tests example"; do
     python3 -m isort $module --profile black
     python3 -m black --line-length 100 --target-version py38 --required-version 22.6.0 $module
     python3 -m flake8 $module
 done
 
-codespell --ignore-regex '(\#.*$|//.*$)' cpp python examples
+codespell --ignore-regex '(\#.*$|//.*$)' cpp src/plainmp tests example
